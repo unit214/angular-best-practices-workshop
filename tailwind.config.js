0b7d26a5db9e2651df2855ec017a33e4c824a3f5
Philipp Piwo <github@piwo.space>
2022-10-05 19:10:25 +0200

    define tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      "color-bg": "#ffeee5",
+      "color-primary": "#a46c00",
+      black: "#000000",
+      white: "#ffffff",
+      gray: "#c6c6c6",
+    },
   },
   plugins: [],
 }
