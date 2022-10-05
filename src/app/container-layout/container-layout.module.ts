f2a8ec05810c6cf72f3256b0d0a9838bf34c10ad
Philipp Piwo <github@piwo.space>
2022-10-05 19:12:12 +0200

    feat(components): add sharded input component

--- src/app/container-layout/container-layout.module.ts
+++ src/app/container-layout/container-layout.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ContainerLayoutRoutingModule } from './container-layout-routing.module';
 import { ContainerLayoutComponent } from './container-layout.component';
 import { HeadingComponent } from "../shared/components/heading/heading.component";
+import { InputComponent } from "../shared/components/input/input.component";
 
 @NgModule({
   declarations: [
     ContainerLayoutComponent,
     HeadingComponent,
+    InputComponent,
   ],
   imports: [
     CommonModule,
