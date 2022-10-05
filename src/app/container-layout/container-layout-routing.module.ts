61c3033afadbe1d71c49603251c86b719118667a
Philipp Piwo <github@piwo.space>
2022-10-05 19:14:44 +0200

    add login and success pages

--- src/app/container-layout/container-layout-routing.module.ts
+++ src/app/container-layout/container-layout-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContainerLayoutComponent } from './container-layout.component';
+import { LoginComponent } from "./pages/login/login.component";
+import { SuccessComponent } from "./pages/success/success.component";
 
 const routes: Routes = [
   {
     path: '',
     component: ContainerLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'success', component: SuccessComponent }
+    ]
   },
 ];
 
--- src/app/container-layout/container-layout.module.ts
+++ src/app/container-layout/container-layout.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ContainerLayoutRoutingModule } from './container-layout-routing.module';
 import { ContainerLayoutComponent } from './container-layout.component';
-import { HeadingComponent } from "../shared/components/heading/heading.component";
-import { InputComponent } from "../shared/components/input/input.component";
-import { ButtonComponent } from "../shared/components/button/button.component";
+import { HeadingComponent } from '../shared/components/heading/heading.component';
+import { InputComponent } from '../shared/components/input/input.component';
+import { ButtonComponent } from '../shared/components/button/button.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SuccessComponent } from './pages/success/success.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { ButtonComponent } from "../shared/components/button/button.component";
     HeadingComponent,
     InputComponent,
     ButtonComponent,
+    LoginComponent,
+    SuccessComponent
   ],
   imports: [
     CommonModule,
