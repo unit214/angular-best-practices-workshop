e34d13c704e5e2769f6d1edbc06f0912ad8a7058
Philipp Piwo <github@piwo.space>
2022-10-05 19:11:31 +0200

    feat(components): add sharded heading component

--- src/app/container-layout/container-layout.module.ts
+++ src/app/container-layout/container-layout.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ContainerLayoutRoutingModule } from './container-layout-routing.module';
 import { ContainerLayoutComponent } from './container-layout.component';
-
+import { HeadingComponent } from "../shared/components/heading/heading.component";
 
 @NgModule({
   declarations: [
     ContainerLayoutComponent,
+    HeadingComponent,
   ],
   imports: [
     CommonModule,
